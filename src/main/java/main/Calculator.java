7b1699585495dbb4b8faca6834f2abe04d59998c
user_2 <user_2@FACE-52>
2019-09-16 15:19:56 +0530

    Calculator

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Calculator {
     public static void main(String[] args)
     {
+    	//
         System.out.println("Enter first and second number:");
         Scanner inp= new Scanner(System.in);
         int num1,num2;
@@ -38,26 +39,25 @@ public class Calculator {
     public static int add(int x, int y)
     {
     	int result=0;
-//        write your code here 
-    	
-        return result;
+        result=x+y;
+    	return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
-//        write your code here
+        result = x - y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+        result=x*y;       
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
-//      write your code here
+        result=x/y;
     	return result;
     }
 
